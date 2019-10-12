51c3686d6a5d86e366d2cfb0dbee6473f044abea
Miller <garrett.miller@kroger.com>
2019-10-12 11:29:41 -0400

    Ingredient form update

--- client/recipe/src/app/components/create-recipe/create-recipe.component.ts
+++ client/recipe/src/app/components/create-recipe/create-recipe.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder} from "@angular/forms";
+import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
 import {CategoryRestService} from "../../services/category-service/category-rest.service";
 import {Category} from "../../models/category";
+import {Recipe} from "../../models/Recipe";
+import {Ingredient} from "../../models/ingredient";
 
 @Component({
   selector: 'app-create-recipe',
@@ -10,20 +12,41 @@ import {Category} from "../../models/category";
 })
 export class CreateRecipeComponent implements OnInit {
 
-  private recipeForm;
+  recipeForm: FormGroup;
   private categoryList: Array<Category>;
 
   constructor(private formBuilder: FormBuilder,
-              private categoryService: CategoryRestService) {
-    this.recipeForm = this.formBuilder.group({
-      recipeName: '',
-      category: '',
-
-    });
-  }
+              private categoryService: CategoryRestService) {}
 
   ngOnInit() {
     this.getCategories();
+    this.recipeForm = this.formBuilder.group({
+      recipeName: '',
+      category: '',
+      ingredientList: this.formBuilder.array([
+        this.formBuilder.group({
+          name:'',
+          measurement: '',
+          unit: ''
+        })
+      ])
+    });
+  }
+
+  get ingredients() {
+    return this.recipeForm.get('ingredientList') as FormArray;
+  }
+
+  addIngredient() {
+    this.ingredients.push(this.formBuilder.group({
+      name:'',
+      measurement: '',
+      unit: ''
+    }));
+  }
+
+  deleteIngredient(index) {
+    this.ingredients.removeAt(index);
   }
 
   public getCategories(){
